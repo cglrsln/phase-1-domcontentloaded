c70ada52387c1f51bdf69ec5fa624c247a7269e0
Caglar <caglarsulun@hotmail.com>
2021-12-02 19:38:08 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    //console.log('DOM has been loaded successfully and then this message appeared.')
+    let p = document.querySelector("#text");
+    p.textContent = "This is really cool!"
+})
+
